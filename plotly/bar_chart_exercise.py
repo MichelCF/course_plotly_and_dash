99333de62f7035851e4b1ac100f53e3b9780c302
Michel Caiafa <michel.caiafa@gmail.com>
2023-01-22 16:56:10 -0300

    Exercise 2.

---
+++ plotly/bar_chart_exercise.py
@@ -0,0 +1,17 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('data/mocksurvey.csv', index_col=0)
+
+#Vertical
+#data = [go.Bar(x=df.index, y=df[response],name=response) for response in df.columns]
+
+#Horizontal
+data = [go.Bar(x=df[response], y=df.index,name=response,orientation='h') for response in df.columns]
+
+layout = go.Layout(title='Questions', barmode='stack')
+
+fig = go.Figure(data=data,layout=layout)
+
+pyo.plot(fig,filename='HTML/BarChartExercise.html')
