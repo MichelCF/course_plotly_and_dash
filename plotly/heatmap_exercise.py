cb7ab0fdc1857539d67f2a79a893a7e2e172c580
Michel Caiafa <michel.caiafa@gmail.com>
2023-01-31 16:01:27 -0300

    Heatmaps.

---
+++ plotly/heatmap_exercise.py
@@ -0,0 +1,15 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('data/flights.csv')
+
+data = [go.Heatmap(x=df['year'],y=df['month'],
+        z=df['passengers'].values.tolist(),
+        colorscale='Jet')]
+
+layout = go.Layout(title ='Fligths')
+
+fig = go.Figure(data=data,layout=layout)
+
+pyo.plot(fig, filename='HTML/Heatmap_Exercise.html')
